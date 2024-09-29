651decb2959ef03a24fb2abbc38ce650e49835a0
“MendsAleixo” <mendelson.aleixo@gmail.com>
2024-09-29 18:11:48 -0300

    Criando a classe Cd
    Implementando a classe CD,  adiconando genero musical às informaçoes

---
+++ src/model/Cd.ts
@@ -0,0 +1,27 @@
+import { Midia } from "./Midia";
+
+export class Cd extends Midia {
+  private _generoMusical: string;
+  constructor(
+    titulo: string,
+    autor: string,
+    ano: number,
+    localizacao: string,
+    duracao: number
+  ) {
+    super(titulo, autor, ano, localizacao, duracao);
+    this._generoMusical = "";
+  }
+
+  getDescricao(): string {
+    return `CD: ${this._titulo}, Autor: ${this._autor}, Ano: ${this._ano}, Genero: ${this._generoMusical}, Duração: ${this._duracao} min.`;
+  }
+
+  getGeneroMusical(): string {
+    return this._generoMusical;
+  }
+
+  setGeneroMusical(generoMusical: string): void {
+    this._generoMusical = generoMusical;
+  }
+}
